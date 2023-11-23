49d3e92ec6fb645c432f70c8ff24f8b16fad43d7
Alexandre <xandetonin@gmail.com>
2023-11-22 22:40:26 -0300

    header adjusts

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,23 +1,20 @@
 import { Link } from 'react-router-dom'
 import reactLogo from '/react.svg'
-import githubAnimation from '../../../public/github.json'
-import Lottie from 'lottie-react'
-
+import { IoMoonOutline, IoSunnyOutline } from "react-icons/io5";
 
 export default function Header() {
     return (
-        <header className="h-20 max-w-screen shadow-[0px_0px_5px_2px_rgba(0,0,0,0.1)] px-4 flex items-center justify-between">
-            <div className='flex gap-4 items-center text-lg font-medium h-full'>
-                <Link to={"/"}><img src={reactLogo} alt="" className='w-10 hover:rotate-180 transition-all duration-700' /></Link>
-                <h1>React Hooks </h1>
-            </div>
-
-            <a href="https://github.com/alexandretonin" target='_blank' className='hover:text-[#969696] transition'>
-                <div className='gap-4 flex items-center'>
-                    <p>github/AlexandreTonin</p>
-                    <Lottie animationData={githubAnimation} alt="" className='w-8' />
+        <header className='w-screen shadow-[0px_0px_5px_1px_rgba(0,0,0,0.1)] bg-white sticky top-0'>
+            <div className="h-16 lg:h-20 max-w-screen container mx-auto px-4 flex items-center justify-between">
+                <div className='flex gap-4 items-center text-lg font-medium h-full'>
+                    <Link to={"/"}><img src={reactLogo} alt="" className='w-10 hover:rotate-180 transition-all duration-700' /></Link>
+                    <h1>React Hooks </h1>
                 </div>
-            </a>
+                <div className='flex lg:text-3xl text-2xl text-[#404040] rounded-full'>
+                    <span className='hover:text-yellow-400 transition cursor-pointer'><IoSunnyOutline /></span>
+                    <span className='hover:text-yellow-500 transition cursor-pointer'><IoMoonOutline /></span>
+                </div>
+            </div>
         </header>
 
 
